e5d504b9f1b9112ecab1af159893554c82103f0c
L <60060180+GojouSats@users.noreply.github.com>
2022-07-18 00:54:18 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+from Pyrostring.__version__ import __Pyrostring__ 
+
+with open("requirements.txt") as requirements_txt:
+    requirements = requirements_txt.read().splitlines()
+
+
+setup(
+      name='Pyrostring',
+      version=__Pyrostring__,
+      description='Pyrogram Fast Session String Generator',
+      author='GojouSats',
+      url='https://github.com/GojouSats/Pyrostring',
+      packages=requirements,
+     )    
+    
